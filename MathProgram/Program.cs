c23fd7657972e1b1b97676c949439dd1bbc80396
razbroc <razbroc@gmail.com>
2023-02-07 16:38:03 +0200

    add(second assignment, calculator)

--- MathProgram/Program.cs
+++ MathProgram/Program.cs
@@ -1,3 +1,41 @@
-﻿/*
- * TODO: Write code here
- */
+﻿static int Addition(int x, int y)
+{
+return(x + y);
+}
+
+static int Subtraction(int x, int y)
+{
+    return(x - y);
+}
+
+static double Division(double x, double y)
+{
+    return(x / y);
+}
+
+static int Multiplication(int x, int y)
+{
+    return(x * y);
+}
+
+static void Calc(int x, int y, string sign)
+{
+    switch (sign)
+    {
+        case "+":
+            Console.WriteLine(Addition(x,y));
+            break;
+        case "-":
+            Console.WriteLine(Subtraction(x,y));
+            break;
+        case "*":
+            Console.WriteLine(Multiplication(x,y));
+            break;
+        case "/":
+            Console.WriteLine(Division(x,y));
+            break;
+        case "^":
+            Console.WriteLine(Math.Pow(x,y));
+            break;
+    }
+}

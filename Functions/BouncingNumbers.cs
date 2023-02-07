419f1cba1e604cc9508d4b18d6d6c251ec3d222a
omri2084 <omri2084@cs.huji.ac.il>
2023-02-07 18:09:06 +0200

    ex9

--- Functions/BouncingNumbers.cs
+++ Functions/BouncingNumbers.cs
@@ -5,4 +5,31 @@ public class BouncingNumbers
     /*
 	 * TODO: Write helper function for 'BouncingNumbers'
 	 */
+	public static bool up(int num)
+    {
+
+        string str = num.ToString();
+        for (int i = str.Length - 1; i > 0; i--)
+        {
+            bool h = (int)str[i] >= (int)str[i - 1];
+            if (!h)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static bool down(int num)
+    {
+        string str = num.ToString();
+        for (int i = 0; i < str.Length - 1; i++)
+        {
+            bool h = (int)str[i] >= (int)str[i + 1];
+            if (!h)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }

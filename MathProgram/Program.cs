2805716e33390e41262916f0c2bfefd747f2904a
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2023-02-07 15:14:01 +0200

    Assignment 2

--- MathProgram/Program.cs
+++ MathProgram/Program.cs
@@ -1,3 +1,40 @@
 ﻿/*
  * TODO: Write code here
  */
+
+int num1, num2;
+char op;
+Console.WriteLine("Enter a number");
+string str;
+
+str = Console.ReadLine();
+num1 = Convert.ToInt32(str);
+
+Console.WriteLine("Enter an operator, i.e. : +, -, *, /");
+str = Console.ReadLine();
+
+op = (char)str[0];
+
+Console.WriteLine("Enter a second number");
+str = Console.ReadLine();
+num2 = Convert.ToInt32(str);
+
+double result = 0;
+switch (op)
+{
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+}
+
+Console.WriteLine($"{num1} {op} {num2} is {result}");
+Console.ReadKey();

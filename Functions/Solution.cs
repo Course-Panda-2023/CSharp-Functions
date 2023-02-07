5ddd98c2056e78dddd0f667f8da16fc286baa388
razbroc <razbroc@gmail.com>
2023-02-07 16:08:38 +0200

    add(first assignment)

--- Functions/Solution.cs
+++ Functions/Solution.cs
@@ -13,70 +13,76 @@ namespace Basic
          */
 
         #region Assignment solutions
-        //public static void Assignment1(int low, int high, out int res)
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
+        public static void Assignment1(int low, int high, out int res)
+        {
+            Random rnd = new Random();
+            if (low < high)
+            {
+                res = rnd.Next(low, high);
+            }
+            else 
+            {
+                throw new Exception("input is not valid");
+            }
+        }
 
-        //public static void Assignment3()
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
+        public static void Assignment3()
+        {
+            /*
+             * TODO: Write code here
+             */
+        }
 
-        //public static double[] Assignment4(bool asc, params double[] nums)
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
+        public static double[] Assignment4(bool asc, params double[] nums)
+        {
+            /*
+             * TODO: Write code here
+             */
+        }
 
-        //public static void Assignment5(string str, char[] letters)
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
+        public static void Assignment5(string str, char[] letters)
+        {
+            /*
+             * TODO: Write code here
+             */
+        }
 
-        //public static bool Assignment6(string str)
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
+        public static bool Assignment6(string str)
+        {
+            /*
+             * TODO: Write code here
+             */
+        }
 
-        //public static int Assignment7(int[] nums)
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
+        public static int Assignment7(int[] nums)
+        {
+            /*
+             * TODO: Write code here
+             */
+        }
 
-        //public static void Assignment8(bool[,] board, int rows, int cols)
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
+        public static void Assignment8(bool[,] board, int rows, int cols)
+        {
+            /*
+             * TODO: Write code here
+             */
+        }
 
-        //public static int Assignment9()
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
-        //#endregion
+        public static int Assignment9()
+        {
+            /*
+             * TODO: Write code here
+             */
+        }
+        #endregion
 
-        //#region Bonus solutions
-        //public static int Bonus1()
-        //{
-        //    /*
-        //     * TODO: Write code here
-        //     */
-        //}
+        #region Bonus solutions
+        public static int Bonus1()
+        {
+            /*
+             * TODO: Write code here
+             */
+        }
 
         public static int Bonus2(int[] hightMap)
         {

95b4d6eacfdf1640cb9d775df3244be552baa2d4
liavbatson <batsonliav@gmail.com>
2023-02-07 15:26:33 +0200

    No 8, Bonus, And 5 not working

--- Functions/Solution.cs
+++ Functions/Solution.cs
@@ -15,75 +15,161 @@ namespace Basic
         #region Assignment solutions
         public static void Assignment1(int low, int high, out int res)
         {
-            /*
-             * TODO: Write code here
-             */
+            Random rnd = new Random();
+            res = rnd.Next(low, high);
+            return res;
         }
 
         public static void Assignment3()
         {
-            /*
-             * TODO: Write code here
-             */
+            Console.WriteLine("Enter first player choice S / R / P");
+            string firstPlayer = Console.ReadLine()!;
+            Console.WriteLine("Enter second player choice S / R / P");
+            string secondPlayer = Console.ReadLine()!;
+            RockPaperScissors.RPS firstPlayerChoice = RockPaperScissors.RPS.Rock;
+            RockPaperScissors.RPS secondPlayerChoice = RockPaperScissors.RPS.Rock;
+            switch (firstPlayer)
+            {
+                case "S":
+                    firstPlayerChoice = RockPaperScissors.RPS.Scissors;
+                    break;
+                case "R":
+                    firstPlayerChoice = RockPaperScissors.RPS.Rock;
+                    break;
+                case "P":
+                    firstPlayerChoice = RockPaperScissors.RPS.Paper;
+                    break;
+                default:
+                    break;
+            }
+            switch (secondPlayer)
+            {
+                case "S":
+                    firstPlayerChoice = RockPaperScissors.RPS.Scissors;
+                    break;
+                case "R":
+                    firstPlayerChoice = RockPaperScissors.RPS.Rock;
+                    break;
+                case "P":
+                    firstPlayerChoice = RockPaperScissors.RPS.Paper;
+                    break;
+                default:
+                    break;
+            }
+            if (firstPlayerChoice == RockPaperScissors.RPS.Rock && secondPlayerChoice == RockPaperScissors.RPS.Paper) { Console.WriteLine("Second player wins!"); }
+            else if (firstPlayerChoice == RockPaperScissors.RPS.Rock && secondPlayerChoice == RockPaperScissors.RPS.Rock) { Console.WriteLine("Its a tie!"); }
+            else if (firstPlayerChoice == RockPaperScissors.RPS.Rock && secondPlayerChoice == RockPaperScissors.RPS.Scissors) { Console.WriteLine("First player wins!"); }
+            else if (firstPlayerChoice == RockPaperScissors.RPS.Paper && secondPlayerChoice == SRP.Paper) { Console.WriteLine("Its a tie!"); }
+            else if (firstPlayerChoice == RockPaperScissors.RPS.Paper && secondPlayerChoice == SRP.Rock) { Console.WriteLine("First player wins!"); }
+            else if (firstPlayerChoice == RockPaperScissors.RPS.Paper && secondPlayerChoice == SRP.Scissors) { Console.WriteLine("Second player wins!"); }
+            else if (firstPlayerChoice == RockPaperScissors.RPS.Scissors && secondPlayerChoice == SRP.Paper) { Console.WriteLine("First player wins!"); }
+            else if (firstPlayerChoice == RockPaperScissors.RPS.Scissors && secondPlayerChoice == SRP.Rock) { Console.WriteLine("Second player wins!"); }
+            else if (firstPlayerChoice == RockPaperScissors.RPS.Scissors && secondPlayerChoice == SRP.Scissors) { Console.WriteLine("Its a tie!"); }
         }
 
         public static double[] Assignment4(bool asc, params double[] nums)
         {
-            /*
-             * TODO: Write code here
-             */
+            if (asc)
+            {
+                for (int j = 0; j <= nums.Length - 2; j++)
+                {
+                    for (int i = 0; i <= nums.Length - 2; i++)
+                    {
+                        if (nums[i] > nums[i + 1])
+                        {
+                            double temp = nums[i + 1];
+                            nums[i + 1] = nums[i];
+                            nums[i] = temp;
+                        }
+                    }
+                }
+                return nums;
+            }
+            else
+            {
+                for (int j = 0; j <= nums.Length - 2; j++)
+                {
+                    for (int i = 0; i <= nums.Length - 2; i++)
+                    {
+                        if (nums[i] < nums[i + 1])
+                        {
+                            double temp = nums[i + 1];
+                            nums[i + 1] = nums[i];
+                            nums[i] = temp;
+                        }
+                    }
+                }
+                return nums;
+            }
         }
 
         public static void Assignment5(string str, char[] letters)
         {
-            /*
-             * TODO: Write code here
-             */
+            foreach (char l in letters)
+            {
+                str.Replace(l, (char)((char)Convert.ToInt16(l) + 10));
+            }
         }
 
         public static bool Assignment6(string str)
         {
-            /*
-             * TODO: Write code here
-             */
+            string text = Regex.Replace(str, "[^a-zA-Z0-9]", String.Empty);
+            string ret = text.ToLower();
+            string reverseString = string.Empty;
+            for (int i = ret.Length - 1; i >= 0; i--)
+            {
+                reverseString += ret[i];
+            }
+            if (ret == reverseString)
+                return true;
+            return false;
         }
 
         public static int Assignment7(int[] nums)
         {
-            /*
-             * TODO: Write code here
-             */
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int occ = nums.Count(x => x == nums[i]);
+                if (occ == 1)
+                    return nums[i];
+            }
+            return -1;
         }
 
-        public static void Assignment8(bool[,] board, int rows, int cols)
+        /*public static void Assignment8(bool[,] board, int rows, int cols)
         {
-            /*
-             * TODO: Write code here
-             */
-        }
+            
+        }*/
 
-        public static int Assignment9()
+        public static int Assignment9(int num)
         {
-            /*
-             * TODO: Write code here
-             */
+            bool flag = false;
+            string str = num.ToString();
+            char[] chars = str.ToCharArray();
+            chars = chars.OrderBy(c => c).ToArray();
+            string sorted = new string(chars);
+            if (str == sorted)
+                flag = true;
+            char[] c = str.ToCharArray();
+            c = c.OrderByDescending(c => c).ToArray();
+            string decSorted = new string(c);
+            if (str == decSorted)
+                flag = true;
+            return flag;
         }
         #endregion
-
-        #region Bonus solutions
+        
+        /*#region Bonus solutions
         public static int Bonus1()
         {
-            /*
-             * TODO: Write code here
-             */
+
         }
 
         public static int Bonus2(uint[] hightMap)
         {
-            /*
-             * TODO: Write code here
-             */
+
         }
         #endregion
+        */
     }
 }
--- MathProgram/Program.cs
+++ MathProgram/Program.cs
@@ -1,3 +1,24 @@
-﻿/*
- * TODO: Write code here
- */
+﻿public static void EX2(double firstNumber, double secondNumber, string operand)
+{
+    switch (operand)
+    {
+        case "+":
+            Console.WriteLine(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+            break;
+        case "-":
+            Console.WriteLine(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+            break;
+        case "*":
+            Console.WriteLine(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+            break;
+        case "/":
+            Console.WriteLine(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
+            break;
+        case "^":
+            int power = (int)Math.Pow(firstNumber, secondNumber);
+            Console.WriteLine(firstNumber + " ^ " + secondNumber + " = " + power);
+            break;
+        default:
+            break;
+    }
+}

c0faecc489a9be2ebc5c74de7ccb71180fd0d99d
razbroc <razbroc@gmail.com>
2023-02-07 18:51:25 +0200

    add(count neigbors in gameOfLife assignment 8)

--- Functions/GameOfLife.cs
+++ Functions/GameOfLife.cs
@@ -2,7 +2,27 @@
 
 public class GameOfLife
 {
-	/*
-	 * TODO: Write helper function for 'Game of life'
-	 */
+    public static int CountNeighbors(int[,] board, int cellIndexRow, int cellIndexCol)
+    {
+        int aliveCounter = 0;
+        for (int rowIndex = cellIndexRow - 1; rowIndex <= cellIndexRow + 1; rowIndex++)
+        {
+            if (rowIndex > board.GetLength(0) - 1 || rowIndex == -1)
+            {
+                continue;
+            }
+            for (int colIndex = cellIndexCol - 1; colIndex <= cellIndexCol; colIndex++)
+            {
+                if (colIndex > board.GetLength(1) - 1 || colIndex == -1)
+                {
+                    continue;
+                }
+                if (board[rowIndex,colIndex] == 1 && (rowIndex != cellIndexRow || cellIndexCol != colIndex))
+                {
+                    aliveCounter++;
+                }
+            }
+        }
+        return aliveCounter;
+    }
 }

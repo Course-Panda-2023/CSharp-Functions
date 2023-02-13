af9fa22a80a4b3c5ae9398e1d01fc71dd810fe2e
Tamir <tamirk07gmail.com>
2023-02-13 18:01:30 +0200

    Assignment1+3 (messed up the first commit of A1)

--- Functions/Solution.cs
+++ Functions/Solution.cs
@@ -3,28 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Basic.Helper
 
 namespace Basic
 {
     internal class Solution
     {
+        private static int p2=0;
+        private static int p1=0;
+
         /*
-         * Notice: Write helper functions in `Helper` class unless there is a special class for it
-         */
+* Notice: Write helper functions in `Helper` class unless there is a special class for it
+*/
 
         #region Assignment solutions
         public static void Assignment1(int low, int high, out int res)
         {
-            /*
-             * TODO: Write code here
-             */
+            Random ran = new Random();
+            res = ran.Next(low, high);
         }
 
         public static void Assignment3()
         {
-            /*
-             * TODO: Write code here
-             */
+            string? playerInput1 = Console.ReadLine();
+            string? playerInput2 = Console.ReadLine();
+            
+            if (playerInput1 == "Paper")
+            {
+                p1 = (int) RockPaperScissors.RPS.Paper;
+            }
+            
+            if (playerInput1 == "Scissors")
+            {
+                p1 = (int) RockPaperScissors.RPS.Scissors;
+            }
+
+            if (playerInput2 == "Paper")
+            {
+                p2 = (int) RockPaperScissors.RPS.Paper;
+            }
+
+            if (playerInput2 == "Scissors")
+            {
+                p2 = (int) RockPaperScissors.RPS.Scissors;
+            }
+            
+            int d=p1-p2;
+            if (d<0)
+            {
+                d+=3;
+            }
+
+            if (d == 0)
+            {
+                Console.WriteLine("It's a tie!");
+            }
+
+            if (d == 1)
+            {
+                Console.WriteLine("First player wins!");
+            }
+
+            if (d == 2)
+            {
+                Console.WriteLine("Second player wins!");
+            }
+
         }
 
         public static double[] Assignment4(bool asc, params double[] nums)
